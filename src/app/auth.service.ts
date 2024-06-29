5bed44a085fdb7e8d638e7a28361f60b07a7a563
Mariana <mariznee@gmail.com>
2024-06-28 23:27:16 -0300

    add service and guard for login

---
+++ src/app/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3001/auth/login';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { username, password });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
